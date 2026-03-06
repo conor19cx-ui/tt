f9807391d0db7a00429bad3047ecb7e54e2f6224
conor19cx-ui <conor19cx.email.com>
2026-03-07 00:04:57 +0800

    feat:添加Spring Boot项目代码和Hello接口

---
+++ src/main/java/com/example/test_3_6pm/Test36pmApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_3_6pm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test36pmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test36pmApplication.class, args);
+    }
+
+}
